f26bff17f2c3b087b329a07a827b9541722bfb0e
JiefengLi <jeff.lee.sjtu@gmail.com>
2018-09-29 15:50:16 +0800

    Update coco settings

--- train_sppe/src/utils/dataset/coco.py
+++ train_sppe/src/utils/dataset/coco.py
@@ -27,7 +27,7 @@ class Mscoco(data.Dataset):
         self.label_type = label_type
 
         self.nJoints_coco = 17
-        self.nJoints = 33
+        self.nJoints = 17
 
         self.accIdxs = (1, 2, 3, 4, 5, 6, 7, 8,
                         9, 10, 11, 12, 13, 14, 15, 16, 17)
