d82d346cad040bdf0abdc8db7dba8f0ffe96f439
Chen Xiaowei <loongchen25@126.com>
2019-01-08 15:34:20 +0800

    1. fix " unsupported operand type(s) for //: 'Tensor' and 'int'" error.

--- SPPE/src/utils/img.py
+++ SPPE/src/utils/img.py
@@ -1,25 +1,15 @@
-# -----------------------------------------------------
-# Copyright (c) Shanghai Jiao Tong University. All rights reserved.
-# Written by Jiefeng Li (jeff.lee.sjtu@gmail.com)
-# -----------------------------------------------------
-
 import numpy as np
+import cv2
 import torch
 import scipy.misc
-from torchsample.transforms import SpecialCrop, Pad
+from torchvision import transforms
 import torch.nn.functional as F
-import cv2
-from opt import opt
 
-
-RED = (0, 0, 255)
-GREEN = (0, 255, 0)
-BLUE = (255, 0, 0)
-CYAN = (255, 255, 0)
-YELLOW = (0, 255, 255)
-ORANGE = (0, 165, 255)
-PURPLE = (255, 0, 255)
-
+from PIL import Image
+from copy import deepcopy
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
 def im_to_torch(img):
     img = np.transpose(img, (2, 0, 1))  # C*H*W
@@ -107,7 +97,7 @@ def drawGaussian(img, pt, sigma):
     x = np.arange(0, size, 1, float)
     y = x[:, np.newaxis]
     x0 = y0 = size // 2
-    sigma = size / 4.0
+    sigma = size / 4.0    
     # The gaussian is not normalized, we want the center value to equal 1
     g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * sigma ** 2))
 
@@ -252,36 +242,73 @@ def transformBoxInvert_batch(pt, ul, br, inpH, inpW, resH, resW):
 
 def cropBox(img, ul, br, resH, resW):
     ul = ul.int()
-    br = br.int()
-    lenH = max(br[1] - ul[1], (br[0] - ul[0]) * resH / resW)
+    br = (br - 1).int()
+    # br = br.int()
+    lenH = max((br[1] - ul[1]).item(), (br[0] - ul[0]).item() * resH / resW)
     lenW = lenH * resW / resH
     if img.dim() == 2:
         img = img[np.newaxis, :]
 
-    newDim = torch.IntTensor((img.size(0), int(lenH), int(lenW)))
-    newImg = img[:, ul[1]:, ul[0]:].clone()
-    # Crop and Padding
-    size = torch.IntTensor((int(br[1] - ul[1]), int(br[0] - ul[0])))
-    newImg = SpecialCrop(size, 1)(newImg)
-    newImg = Pad(newDim)(newImg)
-    # Resize to output
-    v_Img = torch.autograd.Variable(newImg)
-    v_Img = torch.unsqueeze(v_Img, 0)
-    # newImg = F.upsample_bilinear(v_Img, size=(int(resH), int(resW))).data[0]
-    if torch.__version__ == '0.4.0a0+32f3bf7' or torch.__version__ == '0.4.0':
-        newImg = F.upsample(v_Img, size=(int(resH), int(resW)),
-                            mode='bilinear', align_corners=True).data[0]
-    else:
-        newImg = F.interpolate(v_Img, size=(int(resH), int(resW)),
-                               mode='bilinear', align_corners=True).data[0]
-    return newImg
+    box_shape = [br[1] - ul[1], br[0] - ul[0]]
+    pad_size = [int((lenH - box_shape[0]) / 2), 
+                int((lenW - box_shape[1]) / 2)]
+    # Padding Zeros
+    img[:, :ul[1], :], img[:, :, :ul[0]] = 0, 0
+    img[:, br[1] + 1:, :], img[:, :, br[0] + 1:] = 0, 0
+
+    src = np.zeros((3, 2), dtype=np.float32)
+    dst = np.zeros((3, 2), dtype=np.float32)
+
+    src[0, :] = np.array(
+        [ul[0] - pad_size[1], ul[1] - pad_size[0]], np.float32)
+    src[1, :] = np.array(
+        [br[0] + pad_size[1], br[1] + pad_size[0]], np.float32)
+    dst[0, :] = 0
+    dst[1, :] = np.array([resW - 1, resH - 1], np.float32)
+
+    src[2:, :] = get_3rd_point(src[0, :], src[1, :])
+    dst[2:, :] = get_3rd_point(dst[0, :], dst[1, :])
+
+    trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+    dst_img = cv2.warpAffine(torch_to_im(img), trans,
+                             (resW, resH), flags=cv2.INTER_LINEAR)
+
+    return im_to_torch(torch.Tensor(dst_img))
 
 
-def flip_v(x, cuda=False):
+def cv_rotate(img, rot, resW, resH):
+    center = np.array((resW - 1, resH - 1)) / 2
+    rot_rad = np.pi * rot / 180
+
+    src_dir = get_dir([0, (resH - 1) * -0.5], rot_rad)
+    dst_dir = np.array([0, (resH - 1) * -0.5], np.float32)
+
+    src = np.zeros((3, 2), dtype=np.float32)
+    dst = np.zeros((3, 2), dtype=np.float32)
+
+    src[0, :] = center
+    src[1, :] = center + src_dir
+    dst[0, :] = [(resW - 1) * 0.5, (resH - 1) * 0.5]
+    dst[1, :] = np.array([(resW - 1) * 0.5, (resH - 1) * 0.5]) + dst_dir
+
+    src[2:, :] = get_3rd_point(src[0, :], src[1, :])
+    dst[2:, :] = get_3rd_point(dst[0, :], dst[1, :])
+
+    trans = cv2.getAffineTransform(np.float32(src), np.float32(dst))
+
+    dst_img = cv2.warpAffine(torch_to_im(img), trans,
+                             (resW, resH), flags=cv2.INTER_LINEAR)
+
+    return im_to_torch(torch.Tensor(dst_img))
+
+
+
+def flip_v(x, cuda=True, volatile=True):
     x = flip(x.cpu().data)
     if cuda:
-        x = x.cuda()
-    x = torch.autograd.Variable(x)
+        x = x.cuda(async=True)
+    x = torch.autograd.Variable(x, volatile=volatile)
     return x
 
 
@@ -325,67 +352,82 @@ def shuffleLR(x, dataset):
 def shuffleLR_v(x, dataset, cuda=False):
     x = shuffleLR(x.cpu().data, dataset)
     if cuda:
-        x = x.cuda()
+        x = x.cuda(async=True)
     x = torch.autograd.Variable(x)
     return x
 
 
-def vis_frame(frame, im_res, format='coco'):
-    '''
-    frame: frame image
-    im_res: im_res of predictions
-    format: coco or mpii
+def drawMPII(inps, preds):
+    assert inps.dim() == 4
+    p_color = ['g', 'b', 'purple', 'b', 'purple',
+               'y', 'o', 'y', 'o', 'y', 'o',
+               'pink', 'r', 'pink', 'r', 'pink', 'r']
+    p_color = ['r', 'r', 'r', 'b', 'b', 'b',
+               'black', 'black', 'black', 'black',
+               'y', 'y', 'white', 'white', 'g', 'g']
 
-    return rendered image
-    '''
-    if format == 'coco':
-        l_pair = [
-            (0, 1), (0, 2), (1, 3), (2, 4),  # Head
-            (5, 6), (5, 7), (7, 9), (6, 8), (8, 10),
-            (5, 11), (6, 12),  # Body
-            (11, 13), (12, 14), (13, 15), (14, 16)
-        ]
-        p_color = [RED, RED, RED, RED, RED, YELLOW, YELLOW, YELLOW,
-                   YELLOW, YELLOW, YELLOW, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN]
-        line_color = [YELLOW, YELLOW, YELLOW, YELLOW, BLUE, BLUE,
-                      BLUE, BLUE, BLUE, PURPLE, PURPLE, RED, RED, RED, RED]
-    elif format == 'mpii':
-        l_pair = [
-            (8, 9), (11, 12), (11, 10), (2, 1), (1, 0),
-            (13, 14), (14, 15), (3, 4), (4, 5),
-            (8, 7), (7, 6), (6, 2), (6, 3), (8, 12), (8, 13)
-        ]
-        p_color = [PURPLE, BLUE, BLUE, RED, RED, BLUE, BLUE, RED,
-                   RED, PURPLE, PURPLE, PURPLE, RED, RED, BLUE, BLUE]
-        line_color = [PURPLE, BLUE, BLUE, RED, RED, BLUE, BLUE,
-                      RED, RED, PURPLE, PURPLE, RED, RED, BLUE, BLUE]
-    else:
-        raise NotImplementedError
+    nImg = inps.size(0)
+    imgs = []
+    for n in range(nImg):
+        img = to_numpy(inps[n])
+        img = np.transpose(img, (1, 2, 0))
+        imgs.append(img)
 
-    im_name = im_res['imgname'].split('/')[-1]
-    img = frame.copy()
-    for human in im_res['result']:
-        part_line = {}
-        kp_preds = human['keypoints']
-        kp_scores = human['kp_score']
-        # Draw keypoints
-        for n in range(kp_scores.shape[0]):
-            if kp_scores[n] <= 0.15:
-                continue
-            cor_x, cor_y = int(kp_preds[n, 0]), int(kp_preds[n, 1])
-            part_line[n] = (cor_x, cor_y)
-            cv2.circle(img, (cor_x, cor_y), 4, p_color[n], -1)
-            # Now create a mask of logo and create its inverse mask also
-            #transparency = max(0, min(1, kp_scores[n]))
-            #img = cv2.addWeighted(bg, transparency, img, 1, 0)
-        # Draw limbs
-        for i, (start_p, end_p) in enumerate(l_pair):
-            if start_p in part_line and end_p in part_line:
-                start_xy = part_line[start_p]
-                end_xy = part_line[end_p]
-                cv2.line(img, start_xy, end_xy,
-                         line_color[i], (0.5 * (kp_scores[start_p] + kp_scores[end_p])) + 1)
-                #transparency = max(
-                #    0, min(1, (kp_scores[start_p] + kp_scores[end_p])))
-                #img = cv2.addWeighted(bg, transparency, img, 1, 0)
-    return img
+    fig = plt.figure()
+    plt.imshow(imgs[0])
+    ax = fig.add_subplot(1, 1, 1)
+    #print(preds.shape)
+    for p in range(16):
+        x, y = preds[0][p]
+        cor = (round(x), round(y)), 10
+        ax.add_patch(plt.Circle(*cor, color=p_color[p]))
+    plt.axis('off')
+
+    plt.show()
+
+    return imgs
+
+
+def drawCOCO(inps, preds, scores):
+    assert inps.dim() == 4
+    p_color = ['g', 'b', 'purple', 'b', 'purple',
+               'y', 'orange', 'y', 'orange', 'y', 'orange',
+               'pink', 'r', 'pink', 'r', 'pink', 'r']
+
+    nImg = inps.size(0)
+    imgs = []
+    for n in range(nImg):
+        img = to_numpy(inps[n])
+        img = np.transpose(img, (1, 2, 0))
+        imgs.append(img)
+
+    fig = plt.figure()
+    plt.imshow(imgs[0])
+    ax = fig.add_subplot(1, 1, 1)
+    #print(preds.shape)
+    for p in range(17):
+        if scores[0][p][0] < 0.2:
+            continue
+        x, y = preds[0][p]
+        cor = (round(x), round(y)), 3
+        ax.add_patch(plt.Circle(*cor, color=p_color[p]))
+    plt.axis('off')
+
+    plt.show()
+
+    return imgs
+
+
+def get_3rd_point(a, b):
+    direct = a - b
+    return b + np.array([-direct[1], direct[0]], dtype=np.float32)
+
+
+def get_dir(src_point, rot_rad):
+    sn, cs = np.sin(rot_rad), np.cos(rot_rad)
+
+    src_result = [0, 0]
+    src_result[0] = src_point[0] * cs - src_point[1] * sn
+    src_result[1] = src_point[0] * sn + src_point[1] * cs
+
+    return src_result
