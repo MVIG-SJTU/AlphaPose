6901bbb45bb358710e14e153e3cdec11c42de1d7
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2019-02-26 13:39:51 -0800

    Update fetch_models.sh
    Made simpler

--- fetch_models.sh
+++ fetch_models.sh
@@ -1,10 +1,8 @@
-cd human-detection
+#get res152
 wget mvig.sjtu.edu.cn/publications/rmpe/output.zip
-unzip output.zip
+unzip -d output.zip human-detection
 rm output.zip
-cd ../predict
-mkdir models
-cd models
-wget mvig.sjtu.edu.cn/publications/rmpe/final_model.t7
-cd ../..
 
+#get model
+mkdir -p predict/models
+wget mvig.sjtu.edu.cn/publications/rmpe/final_model.t7 -P predict/models
