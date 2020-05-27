0ec58b1f02d0354e72df6aba6b61899c99b29a9e
xuchao1688 <1209789573@qq.com>
2020-05-27 23:52:47 +0800

    fix effdet format bug

--- detector/effdet_api.py
+++ detector/effdet_api.py
@@ -107,13 +107,29 @@ class EffDetDetector(BaseDetector):
             scaling_factors = torch.FloatTensor([1./min(self.inp_dim / orig_dim[0], self.inp_dim / orig_dim[1]) for orig_dim in orig_dim_list]).view(-1, 1)
             scaling_factors = scaling_factors.to(args.device) if args else scaling_factors.cuda()
             prediction = self.model(imgs, scaling_factors) 
-            #do nms to the detection results, only human category is left
-            dets = self.dynamic_get_results(prediction, self.confidence, 
-                                              self.num_classes, nms=True, 
-                                              nms_conf=self.nms_thres)
-            if isinstance(dets, int) or dets.shape[0] == 0:
+            #change the pred format to alphapose (nms has already been done in effdeteval model)
+            prediction = prediction.cpu()
+            write = False
+            for index, sample in enumerate(prediction):
+                for det in sample:
+                    score = float(det[4])
+                    if score < .001:  # stop when below this threshold, scores in descending order
+                        break
+                    if int(det[5]) != 1 or score < self.confidence:
+                        continue
+                    det_new = prediction.new(1,8)
+                    det_new[0,0] = index    #index of img
+                    det_new[0,1:3] = det[0:2]  # bbox x1,y1
+                    det_new[0,3:5] = det[0:2] + det[2:4] # bbox x2,y2
+                    det_new[0,6:7] = det[4]  # cls conf
+                    det_new[0,7] = det[5]   # cls idx
+                    if not write:
+                        dets = det_new
+                        write = True
+                    else:
+                        dets = torch.cat((dets, det_new))    
+            if not write:
                 return 0
-            dets = dets.cpu()
 
             orig_dim_list = torch.index_select(orig_dim_list, 0, dets[:, 0].long())
             for i in range(dets.shape[0]):
@@ -121,131 +137,6 @@ class EffDetDetector(BaseDetector):
                 dets[i, [2, 4]] = torch.clamp(dets[i, [2, 4]], 0.0, orig_dim_list[i, 1])
 
             return dets
-
-    def dynamic_get_results(self, prediction, confidence, num_classes, nms=True, nms_conf=0.4):
-        prediction_bak = prediction.clone()
-        dets = self.get_results(prediction.clone(), confidence, num_classes, nms, nms_conf)
-        if isinstance(dets, int):
-            return dets
-
-        if dets.shape[0] > 100:
-            nms_conf -= 0.05
-            dets = self.get_results(prediction_bak.clone(), confidence, num_classes, nms, nms_conf)
-
-        return dets
-
-    def get_results(self, prediction, confidence, num_classes, nms=True, nms_conf=0.4):
-        args = self.detector_opt
-        #prediction: (batchsize, num of objects, (xc,yc,w,h,box confidence, 80 class scores))
-        conf_mask = (prediction[:, :, 4] > confidence).float().float().unsqueeze(2)
-        prediction = prediction * conf_mask
-
-        try:
-            ind_nz = torch.nonzero(prediction[:,:,4]).transpose(0,1).contiguous()
-        except:
-            return 0
-
-        #the 3rd channel of prediction: (xmin,ymin,w,h)->(x1,y1,x2,y2)
-        box_a = prediction.new(prediction.shape)
-        box_a[:,:,0] = prediction[:,:,0]
-        box_a[:,:,1] = prediction[:,:,1]
-        box_a[:,:,2] = prediction[:,:,0] + prediction[:,:,2]
-        box_a[:,:,3] = prediction[:,:,1] + prediction[:,:,3]
-        prediction[:,:,:4] = box_a[:,:,:4]
-
-        batch_size = prediction.size(0)
-
-        output = prediction.new(1, prediction.size(2) + 1)
-        write = False
-        num = 0
-        for ind in range(batch_size):
-            #select the image from the batch
-            image_pred = prediction[ind]
-
-            #Get the class having maximum score, and the index of that class
-            #Get rid of num_classes softmax scores 
-            #Add the class index and the class score of class having maximum score
-            max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
-            max_conf = max_conf.float().unsqueeze(1)
-            max_conf_score = max_conf_score.float().unsqueeze(1)
-            seq = (image_pred[:,:5], max_conf, max_conf_score)
-            #image_pred:(n,(x1,y1,x2,y2,c,s,idx of cls))
-            image_pred = torch.cat(seq, 1)
-
-            #Get rid of the zero entries
-            non_zero_ind =  (torch.nonzero(image_pred[:,4]))
-
-            image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
-
-            #Get the various classes detected in the image
-            try:
-                img_classes = unique(image_pred_[:,-1])
-            except:
-                continue
-
-            #WE will do NMS classwise
-            #print(img_classes)
-            for cls in img_classes:
-                if cls != 0:
-                    continue
-                #get the detections with one particular class
-                cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
-                class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
-
-                image_pred_class = image_pred_[class_mask_ind].view(-1,7)
-
-                #sort the detections such that the entry with the maximum objectness
-                #confidence is at the top
-                conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
-                image_pred_class = image_pred_class[conf_sort_index]
-                idx = image_pred_class.size(0)
-
-                #if nms has to be done
-                if nms:
-                    if platform.system() != 'Windows':
-                        #We use faster rcnn implementation of nms (soft nms is optional)
-                        nms_op = getattr(nms_wrapper, 'nms')
-                        #nms_op input:(n,(x1,y1,x2,y2,c))
-                        #nms_op output: input[inds,:], inds
-                        _, inds = nms_op(image_pred_class[:,:5], nms_conf)
-
-                        image_pred_class = image_pred_class[inds]
-                    else:
-                        # Perform non-maximum suppression
-                        max_detections = []
-                        while image_pred_class.size(0):
-                            # Get detection with highest confidence and save as max detection
-                            max_detections.append(image_pred_class[0].unsqueeze(0))
-                            # Stop if we're at the last detection
-                            if len(image_pred_class) == 1:
-                                break
-                            # Get the IOUs for all boxes with lower confidence
-                            ious = bbox_iou(max_detections[-1], image_pred_class[1:], args)
-                            # Remove detections with IoU >= NMS threshold
-                            image_pred_class = image_pred_class[1:][ious < nms_conf]
-
-                        image_pred_class = torch.cat(max_detections).data
-
-                #Concatenate the batch_id of the image to the detection
-                #this helps us identify which image does the detection correspond to 
-                #We use a linear straucture to hold ALL the detections from the batch
-                #the batch_dim is flattened
-                #batch is identified by extra batch column
-
-                batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
-                seq = batch_ind, image_pred_class
-                if not write:
-                    output = torch.cat(seq,1)
-                    write = True
-                else:
-                    out = torch.cat(seq,1)
-                    output = torch.cat((output,out))
-                num += 1
-    
-        if not num:
-            return 0
-        #output:(n,(batch_ind,x1,y1,x2,y2,c,s,idx of cls))
-        return output
 
     def detect_one_img(self, img_name):
         """
@@ -273,13 +164,30 @@ class EffDetDetector(BaseDetector):
             scaling_factor = torch.FloatTensor([1/min(self.inp_dim / orig_dim[0], self.inp_dim / orig_dim[1]) for orig_dim in img_dim_list]).view(-1, 1)
             scaling_factor = scaling_factor.to(args.device) if args else scaling_factor.cuda()
             prediction = self.model(img, scaling_factor) 
-            #do nms to the detection results, only human category is left
-            dets = self.dynamic_get_results(prediction, self.confidence,
-                                              self.num_classes, nms=True,
-                                              nms_conf=self.nms_thres)
-            if isinstance(dets, int) or dets.shape[0] == 0:
+            #change the pred format to alphapose (nms has already been done in effdeteval model)
+            prediction = prediction.cpu()
+            write = False
+            for index, sample in enumerate(prediction):
+                for det in sample:
+                    score = float(det[4])
+                    if score < .001:  # stop when below this threshold, scores in descending order
+                        break
+                    if int(det[5]) != 1 or score < self.confidence:
+                        continue
+                    det_new = prediction.new(1,8)
+                    det_new[0,0] = index    #index of img
+                    det_new[0,1:3] = det[0:2]  # bbox x1,y1
+                    det_new[0,3:5] = det[0:2] + det[2:4] # bbox x2,y2
+                    det_new[0,6:7] = det[4]  # cls conf
+                    det_new[0,7] = det[5]   # cls idx
+                    if not write:
+                        dets = det_new
+                        write = True
+                    else:
+                        dets = torch.cat((dets, det_new))          
+            if not write:
                 return None
-            dets = dets.cpu()
+
             img_dim_list = torch.index_select(img_dim_list, 0, dets[:, 0].long())
             for i in range(dets.shape[0]):
                 dets[i, [1, 3]] = torch.clamp(dets[i, [1, 3]], 0.0, img_dim_list[i, 0])
@@ -334,9 +242,9 @@ class EffDetDetector(BaseDetector):
                     score = float(det[4])
                     if score < .001:  # stop when below this threshold, scores in descending order
                         break
-                    #### add by das
-                    if int(det[5]) != 1 or score < self.confidence:
-                        continue
+                    #### uncomment it for only human detection
+                    # if int(det[5]) != 1 or score < self.confidence:
+                    #     continue
                     coco_det = dict(
                         image_id=image_id,
                         bbox=det[0:4].tolist(),
