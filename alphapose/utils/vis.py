1917b26518ce4762869383c2d6c76c0b8011df5a
christw16 <38533075+christw16@users.noreply.github.com>
2020-07-15 14:08:07 +0800

    Update vis.py

--- alphapose/utils/vis.py
+++ alphapose/utils/vis.py
@@ -183,7 +183,7 @@ def vis_frame_fast(frame, im_res, add_bbox=False, format='coco'):
             if start_p in part_line and end_p in part_line:
                 start_xy = part_line[start_p]
                 end_xy = part_line[end_p]
-                cv2.line(img, start_xy, end_xy, line_color[i], 2 * (kp_scores[start_p] + kp_scores[end_p]) + 1)
+                cv2.line(img, start_xy, end_xy, line_color[i], 2 * int(kp_scores[start_p] + kp_scores[end_p]) + 1)
     return img
 
 
