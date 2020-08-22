523a8df32cbf6912b3067f981e0543afb393c82e
Fred Fang <510641426@qq.com>
2020-08-22 16:22:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ def get_install_requires():
         'tqdm', 'tensorboardx', 'easydict',
         'pyyaml',
         'torch>=1.1.0', 'torchvision>=0.3.0',
-        'munkres', 'timm', 'natsort'
+        'munkres', 'timm==0.1.20', 'natsort'
     ]
     # official pycocotools doesn't support Windows, we will install it by third-party git repository later
     if platform.system() != 'Windows':
