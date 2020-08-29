7701e4f3364b087fece1604c72e5b94411573aac
Haoyi Zhu <63538191+HaoyiZhu@users.noreply.github.com>
2020-08-30 00:36:39 +0800

    update datasets

--- alphapose/datasets/custom.py
+++ alphapose/datasets/custom.py
@@ -16,6 +16,29 @@ from pycocotools.coco import COCO
 from alphapose.utils.presets import SimpleTransform
 
 
+import json
+import numpy as np
+import random
+
+def skip_augmentation():
+    x = np.random.rand()
+    if x < 0.3:
+        return True
+    else:
+        return False
+
+def get_bgimg(bgim, box_h, box_w):
+    _bgim = [x for x in bgim if ((x['width'] > box_w) and (x['height'] > box_h))]
+    if len(_bgim) == 0:
+        bgimgpath = random.choice(bgim)['file_name']
+        img = scipy.misc.imread(bgimgpath, mode='RGB')
+        oversize = True
+    else:
+        bgimgpath = random.choice(_bgim)
+        img = scipy.misc.imread(bgimgpath['file_name'], mode='RGB')
+        oversize = False
+    return img, oversize
+
 class CustomDataset(data.Dataset):
     """Custom dataset.
     Annotation file must be in `coco` format.
@@ -90,13 +113,81 @@ class CustomDataset(data.Dataset):
         self._items, self._labels = self._lazy_load_json()
 
     def __getitem__(self, idx):
+        source = None
         # get image id
-        img_path = self._items[idx]
-        img_id = int(os.path.splitext(os.path.basename(img_path))[0])
+        if type(self._items[idx]) == dict:
+            img_path = self._items[idx]['path']
+            img_id = self._items[idx]['id']
+            source = self._items[idx]['source']
+        else:
+            img_path = self._items[idx]
+            img_id = int(os.path.splitext(os.path.basename(img_path))[0])
 
         # load ground truth, including bbox, keypoints, image size
         label = copy.deepcopy(self._labels[idx])
         img = scipy.misc.imread(img_path, mode='RGB')
+
+        if source == 'frei': # hand
+            label['bbox'] = list(label['bbox'])
+            # print(img_path, 'hand augmentation')
+            if not skip_augmentation():
+                bgim = json.load(open('/home/group3/background.json','r'))
+                handkp = label['joints_3d'][:,0:2,0][115:136,:]
+                assert handkp.shape == (21, 2)
+
+                h, w = img.shape[0:2]
+                hand_xmin, hand_xmax, hand_ymin, hand_ymax = int(round(min(handkp[:,0]))), int(round(max(handkp[:,0]))),int(round(min(handkp[:,1]))), int(round(max(handkp[:,1])))
+                boxw_time, boxh_time = float(np.random.random_sample(1)*2+4), float(np.random.random_sample(1)*4+5)
+                box_w, box_h = max(int((hand_xmax - hand_xmin)*boxw_time), w+1), max(int((hand_ymax - hand_ymin)*boxh_time),h+1)
+                
+                background, oversize = get_bgimg(bgim, box_h, box_w)
+                if oversize:
+                    background, _ = get_bgimg(bgim, h, w)
+                    box_h, box_w = background.shape[0:2]
+                    x, y = int(np.random.randint(0,int(box_w - w),size=1)), int(np.random.randint(0,int(box_h - h), size=1))
+                    hd = copy.deepcopy(img)
+                    new_image = copy.deepcopy(background)
+                    new_image[y:y+h, x:x+w, :] = hd
+                    handkp[:,0] = handkp[:,0] + x
+                    handkp[:,1] = handkp[:,1] + y
+                    label['bbox'][0] = label['bbox'][0] + x
+                    label['bbox'][1] = label['bbox'][1] + y
+                    label['bbox'][2] = label['bbox'][2] + x
+                    label['bbox'][3] = label['bbox'][3] + y
+                else:
+                    bh, bw, bc = background.shape
+
+                    x, y = int(np.random.randint(0,int(bw - box_w),size=1)), int(np.random.randint(0,int(bh - box_h), size=1))
+                    hd = copy.deepcopy(img)
+                    new_image = copy.deepcopy(background)
+                    ralative_x, ralative_y = int(np.random.randint(0,int(box_w-w),size=1)), int(np.random.randint(0,int(box_h-h), size=1))
+                    new_loc_x, new_loc_y = x + ralative_x, y + ralative_y
+                    assert  new_loc_x+w < x+box_w
+                    assert  new_loc_y+h < y+box_h
+
+                    handkp[:,0] = handkp[:,0] + new_loc_x
+                    handkp[:, 1] = handkp[:,1] + new_loc_y
+
+                    if new_loc_x < 0:
+                        hd = hd[:,-new_loc_x:,:]
+                        new_loc_x = 0
+                    if new_loc_y < 0:
+                        hd = hd[-new_loc_y:,:,:]
+                        new_loc_y = 0
+                    if new_loc_x+hd.shape[1]>new_image.shape[1]:
+                        hd = hd[:, :new_image.shape[1]-new_loc_x, :]
+                    if new_loc_y+hd.shape[0]>new_image.shape[0]:
+                        hd = hd[:new_image.shape[0]-new_loc_y, :, :]
+
+                    new_image[new_loc_y:new_loc_y+h,new_loc_x:new_loc_x+w,:] = hd
+                    label['bbox'][0] = label['bbox'][0] + new_loc_x
+                    label['bbox'][1] = label['bbox'][1] + new_loc_y
+                    label['bbox'][2] = label['bbox'][2] + new_loc_x
+                    label['bbox'][3] = label['bbox'][3] + new_loc_y
+                label['joints_3d'][:,0:2,0][115:136,:] = handkp
+                img = new_image
+                label['height'], label['width'] = img.shape[0:2]
+            label['bbox'] = tuple(label['bbox'])
 
         # transform ground truth into training label and apply data augmentation
         img, label, label_mask, bbox = self.transformation(img, label)
