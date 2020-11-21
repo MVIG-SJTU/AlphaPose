4066469be2d4209427a87a2c61bc67b6fcb04ecf
Haoyi Zhu <63538191+HaoyiZhu@users.noreply.github.com>
2020-11-21 23:23:11 +0800

    Add files via upload

--- alphapose/datasets/__init__.py
+++ alphapose/datasets/__init__.py
@@ -7,4 +7,4 @@ from .halpe_26 import Halpe_26
 from .halpe_136 import Halpe_136
 from .halpe_136_det import  Halpe_136_det
 from .halpe_26_det import  Halpe_26_det
-__all__ = ['CustomDataset', 'Halpe_136', 'Halpe_26_det', 'Halpe_136_det', 'Halpe_26', 'Mscoco', 'Mscoco_det', 'Mpii', 'ConcatDataset']
+__all__ = ['CustomDataset', 'Halpe_136', 'Halpe_26_det', 'Halpe_136_det', 'Halpe_26', 'Mscoco', 'Mscoco_det', 'Mpii', 'ConcatDataset', 'coco_wholebody', 'coco_wholebody_det']
--- alphapose/datasets/coco_det.py
+++ alphapose/datasets/coco_det.py
@@ -7,7 +7,7 @@
 import json
 import os
 
-import cv2
+import scipy.misc
 import torch
 import torch.utils.data as data
 from tqdm import tqdm
@@ -71,7 +71,7 @@ class Mscoco_det(data.Dataset):
         img_path = './data/coco/val2017/%012d.jpg' % img_id
 
         # Load image
-        image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB) #scipy.misc.imread(img_path, mode='RGB') is depreciated
+        image = scipy.misc.imread(img_path, mode='RGB')
 
         imght, imgwidth = image.shape[1], image.shape[2]
         x1, y1, w, h = det_res['bbox']
