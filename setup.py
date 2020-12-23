63010fcb35b0d4683f3a271094c4a0506474a1e3
Fang-Haoshu <510641426@qq.com>
2020-12-23 23:29:44 +0800

    fix pycocotools version to avoid bug

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ def get_install_requires():
     ]
     # official pycocotools doesn't support Windows, we will install it by third-party git repository later
     if platform.system() != 'Windows':
-        install_requires.append('pycocotools')
+        install_requires.append('pycocotools==2.0.0')
     return install_requires
 
 
